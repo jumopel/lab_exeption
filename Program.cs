725d2592d53328c2f9e741603aab42b5e6a31482
jumopel <tribalka6.gmail.com>
2025-12-08 13:36:32 +0200

    Start of work

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+namespace lab_exeption
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
