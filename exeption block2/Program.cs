3a9cfd8356808451079c4b77c201b09403066fa6
jumopel <tribalka6.gmail.com>
2025-12-16 21:20:04 +0200

    finish 2 block

--- exeption block2/Program.cs
+++ exeption block2/Program.cs
@@ -1,20 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Drawing;
 using System.Threading.Tasks;
 
 namespace exeption_block2
 {
     internal class Program
-    {     
-       
+    {
+
         static void Main(string[] args)
         {
-        }
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string[] files = Directory.GetFiles(currentDirectory);
 
+            Regex regexExtForImage = new Regex(@"\.(bmp|gif|tiff?|jpe?g|png)$",RegexOptions.IgnoreCase);
+
+            foreach (string fileName in files)
+            {
+                string ext = Path.GetExtension(fileName);
+
+                if (!regexExtForImage.IsMatch(ext))
+                    continue;
+                Bitmap bitmap = null;
+
+                try
+                {
+                    bitmap = new Bitmap(fileName);
+                }
+                catch
+                {
+                    Console.WriteLine($"{Path.GetFileName(fileName)} має графічне розширення, але не є зображенням.");
+                    continue;
+                }
+
+                using (bitmap)
+                {
+                    bitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    string nameWithoutExt = Path.GetFileNameWithoutExtension(fileName);
+                    string outputFile = nameWithoutExt + "-mirrored.gif";
+                    string outputPath = Path.Combine(currentDirectory, outputFile);
+                    bitmap.Save(outputPath, ImageFormat.Gif);
+
+                    Console.WriteLine($"Створено: {outputFile}");
+                }
+            }
+
+            Console.WriteLine("Обробку завершено.");
+        }
     }
+        
 }
+
