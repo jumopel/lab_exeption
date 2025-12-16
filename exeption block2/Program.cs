110db6f540b569aa0feb154434523888c6e249f2
jumopel <tribalka6.gmail.com>
2025-12-16 13:25:26 +0200

    Fix structure and add both projects

---
+++ exeption block2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace exeption_block2
+{
+    internal class Program
+    {     
+       
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
